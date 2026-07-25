4293b7616173445a36c2536bb1f0ff9442a7d8ed
Mohamed Hamdhan <hamdhanvkr@gmail.com>
2026-07-25 21:34:18 +0530

    products

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -1,3 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const pool = require("../config/database");
+
+
+// Get all products
+router.get("/", async (req, res) => {
+    try {
+
+        const [products] = await pool.query(
+            "SELECT * FROM products"
+        );
+
+        res.json({
+            success: true,
+            products
+        });
+
+    } catch(error) {
+
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+
+    }
+});
+
 router.post("/", async (req, res) => {
     try {
 
@@ -42,3 +71,6 @@ router.post("/", async (req, res) => {
 
     }
 });
+
+
+module.exports = router;
