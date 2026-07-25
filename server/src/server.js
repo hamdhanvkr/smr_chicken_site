1f6efa3a8ac6cffdf4ccb22c576ddaecfaae3fc0
Mohamed Hamdhan <hamdhanvkr@gmail.com>
2026-07-25 20:24:46 +0530

    check database environment

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,13 @@ const pool = require("./config/database");
 
 const PORT = process.env.PORT || 5000;
 
+console.log({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD ? "YES" : "NO"
+});
+
 async function startServer() {
     try {
         const connection = await pool.getConnection();
@@ -14,7 +21,6 @@ async function startServer() {
         console.error("Database Connection Failed:", error.message);
     }
 
-    // Always start the server
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
