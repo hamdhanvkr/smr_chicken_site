2a0831f330ca5034b5be09bd6fb8a0091ce4d373
Mohamed Hamdhan <hamdhanvkr@gmail.com>
2026-07-26 20:09:23 +0530

    Server Setup

--- server/src/app.js
+++ server/src/app.js
@@ -5,30 +5,34 @@ const path = require("path");
 const app = express();
 
 app.use(cors({
-    origin:[
+    origin: [
         "http://localhost:5173",
         "http://localhost:5174"
     ],
-    credentials:true
+    credentials: true
 }));
 
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// Static uploads
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
+// Routes
 const adminRoutes = require("./routes/admin");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/orders");
-
-
-
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
+// React build
+app.use(express.static(path.join(__dirname, "../dist")));
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
 module.exports = app;
