6a997952716359eb7dc88cbd8c6be457d3cb50a0
Mohamed Hamdhan <hamdhanvkr@gmail.com>
2026-07-28 22:39:36 +0530

    Admin Product  Design

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/adminportal",
+  base: "/adminportal/",
   plugins: [
     react(),
     tailwindcss(),
