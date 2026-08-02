d3a739a1aa547ff8ce7b1bb447398779099050da
Mohamed Hamdhan <hamdhanvkr@gmail.com>
2026-08-02 15:09:56 +0530

    Images

--- server/src/config/initDatabase.js
+++ server/src/config/initDatabase.js
@@ -3,9 +3,6 @@ const pool = require("./database");
 async function initDatabase() {
     try {
 
-
-
-
         await pool.query(`
            CREATE TABLE IF NOT EXISTS categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -37,31 +34,30 @@ async function initDatabase() {
 );
         `);
 
-
-        await pool.query(`
-           CREATE TABLE orders (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(150),
-    phone VARCHAR(20),
-    address TEXT,
-    total DECIMAL(10,2),
-    payment_method VARCHAR(50),
-    payment_status VARCHAR(50),
-    order_status VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-        `);
-        await pool.query(`
-          CREATE TABLE order_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    product_id INT,
-    product_name VARCHAR(255),
-    price DECIMAL(10,2),
-    quantity INT,
-    subtotal DECIMAL(10,2)
-);
-        `);
+//         await pool.query(`
+//            CREATE TABLE orders (
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     customer_name VARCHAR(150),
+//     phone VARCHAR(20),
+//     address TEXT,
+//     total DECIMAL(10,2),
+//     payment_method VARCHAR(50),
+//     payment_status VARCHAR(50),
+//     order_status VARCHAR(50),
+//     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+// );
+//         `);
+//         await pool.query(`
+//           CREATE TABLE order_items (
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     order_id INT,
+//     product_id INT,
+//     product_name VARCHAR(255),
+//     price DECIMAL(10,2),
+//     quantity INT,
+//     subtotal DECIMAL(10,2)
+// );
+//         `);
 
         await pool.query(`
     CREATE TABLE IF NOT EXISTS admins (
