eec06148cd01c2f86e600044c72e53c3182f24e4
Mohamed Hamdhan <hamdhanvkr@gmail.com>
2026-08-02 19:46:39 +0530

    Multer

--- server/src/middleware/upload.js
+++ server/src/middleware/upload.js
@@ -1,23 +1,26 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-
-// Absolute uploads path
-const uploadDir = path.join(__dirname, "../../uploads");
-
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
+
     destination: function (req, file, cb) {
-        cb(null, uploadDir);
+        cb(null, "uploads/");
     },
 
     filename: function (req, file, cb) {
-        const uniqueName = Date.now() + path.extname(file.originalname);
+
+        const uniqueName =
+            Date.now() + path.extname(file.originalname);
+
         cb(null, uniqueName);
     }
+
 });
 
-module.exports = multer({ storage });
+
+const upload = multer({
+    storage: storage
+});
+
+
+module.exports = upload;
