b872f6f78cd54beb5c190c539913758a1b84842f
Mohamed Hamdhan <hamdhanvkr@gmail.com>
2026-08-02 21:24:50 +0530

    Upload

--- server/src/middleware/upload.js
+++ server/src/middleware/upload.js
@@ -1,26 +1,26 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-
-const uploadDir = path.join(process.cwd(), "uploads");
-
-console.log("process.cwd():", process.cwd());
-console.log("uploadDir:", uploadDir);
-
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        console.log("Saving file to:", uploadDir);
-        cb(null, uploadDir);
+
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
     },
-    filename(req, file, cb) {
-        const filename = Date.now() + path.extname(file.originalname);
-        console.log("Filename:", filename);
-        cb(null, filename);
+
+    filename: function (req, file, cb) {
+
+        const uniqueName =
+            Date.now() + path.extname(file.originalname);
+
+        cb(null, uniqueName);
     }
+
 });
 
-module.exports = multer({ storage });
+
+const upload = multer({
+    storage: storage
+});
+
+
+module.exports = upload;
